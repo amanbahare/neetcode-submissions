d4138a1eeada6c7593456a400cee5f66521e6691
amanbahare <154778462+amanbahare@users.noreply.github.com>
2026-09-23 13:25:06 +0530

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 != 0:
+            return False
+        s = list(s)
+        st1 = []
+        for p in s:
+            if p == "(" :
+                st1.append(")") 
+            elif p == "{" :
+                st1.append("}") 
+            elif p == "[" :
+                st1.append("]") 
+            else:
+                if len(st1) != 0 and p == st1[-1]:
+                    st1.pop()
+                else:
+                    return False
+        if len(st1) >= 1:
+            return False
+        return True 
